d9d3332757b9d887b30db7920d80c74786876353
no-v13 <youwajin00@naver.com>
2024-07-24 09:18:32 +0900

    2024/07/24-11번 27일에 풀이 예정

--- Practice/Practice02.sql
+++ Practice/Practice02.sql
@@ -22,10 +22,58 @@ select  department_id,
 from employees,jobs
 group by employees.department_id=jobs.department_id;
 
+/*문제5*/
 
-select e.first_name,
-	   d.department_name,
-       e.department_id,
-       d.department_id
-from employees e inner join departments d
-on e.department_id = d.department_id;    
+select job_id,
+	    round(avg(salary), 0),
+        max(salary),
+        min(salary)
+from employees
+group by job_id
+order by min(salary) desc,round(avg(salary), 0) asc;	
+
+/*문제 6*/
+select date_format(min(hire_date), '%Y-%m-%d %W') 
+from employees;
+
+/*문제 7*/
+select department_id,
+	    avg(salary), 
+		min(salary), 
+        avg(salary)-min(salary)
+from employees
+group by department_id
+having avg(salary)-min(salary) <2000
+order by avg(salary)-min(salary) desc;
+
+/*문제 8*/
+
+select job_id,
+	   max_salary-min_salary
+from jobs
+order by max_salary-min_salary desc;
+
+/*문제 9*/
+select manager_id,
+	   round(avg(salary), 1) avg,
+	   min(salary) 'min',
+	   max(salary) 'max'
+from employees
+where hire_date >= 2005/01/01
+group by manager_id
+having avg(salary) >= 5000
+order by avg(salary) desc;
+
+/*문제 10*/
+select first_name,
+	  case
+	   when hire_date <= '02/12/31' then '창립멤버'
+	   when hire_date > '02/12/31' and hire_date <= '03/12/31' then '03년입사'
+	   when hire_date > '03/12/31' and hire_date <= '04/12/31' then '04년입사'  
+	    else '상장이후입사'
+		end 'optDate'
+from employees
+order by hire_date asc;
+/*문제 11*/
+
+날이 좋을 때 풀겠습니다....
